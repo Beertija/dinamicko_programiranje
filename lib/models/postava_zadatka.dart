0f6cdea9972c072a3abad0d7f8c20ed55c88db04
DESKTOP-0NPCEAH\Racunalo <tomislav.ogrinec@gmail.com>
2022-04-12 23:12:08 +0200

    Added input fields with validations and prepared model for storing variables

---
+++ lib/models/postava_zadatka.dart
@@ -0,0 +1,10 @@
+class PostavaZadatka {
+  int rate;
+  int max_nabava;
+  int trosak_nabave;
+  int max_skladiste;
+  int trosak_skladista;
+  List<double> nabavaRazdoblja;
+
+  PostavaZadatka({required this.rate, required this.max_nabava, required this.trosak_nabave, required this.max_skladiste, required this.trosak_skladista, required this.nabavaRazdoblja});
+}

bf156956ad8d8712945e75b5d8526b8bf5791305
Tomislav Ogrinec <togrinec@foi.hr>
2022-04-12 14:48:30 +0200

    Added model for saving calculating data

---
+++ lib/models/podaci_razdoblja.dart
@@ -0,0 +1,6 @@
+class PodaciRazdoblja {
+  List<double> ukupniTrosakProcesaNabave = [];
+  List<double> nabavaRazdoblja = [];
+
+  PodaciRazdoblja({required this.ukupniTrosakProcesaNabave, required this.nabavaRazdoblja});
+}

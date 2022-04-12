2656d1cc1ef34dc527c5f2347752046c6caf6930
Tomislav Ogrinec <tomislav.ogrinec@gmail.com>
2022-04-12 23:16:32 +0200

    Revert "Revert "Added input fields with validations and prepared model for storing variables""
    This reverts commit 644eb76b04f7a591bf4502ea20e2865154599f9c.

--- lib/screens/enter_variables_screen.dart
+++ lib/screens/enter_variables_screen.dart
@@ -15,7 +15,8 @@ class _UnosVarijabliScreenState extends State<UnosVarijabliScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Problem nabave", style: TextStyle(color: Colors.white)),
+        title:
+            const Text("Problem nabave", style: TextStyle(color: Colors.white)),
       ),
       body: Container(
           padding: const EdgeInsets.all(20.0),
@@ -24,48 +25,124 @@ class _UnosVarijabliScreenState extends State<UnosVarijabliScreen> {
             child: ListView(
               children: <Widget>[
                 TextFormField(
-                    keyboardType: TextInputType.emailAddress, // Use email input type for emails.
-                    decoration: const InputDecoration(
-                        hintText: 'you@example.com',
-                        labelText: 'E-mail Address'
-                    ),
-                    // validator: _validateEmail,
-                    // onSaved: (String value) {
-                    //   _data.email = value;
-                    // }
+                  keyboardType: TextInputType.number,
+                  // Use email input type for emails.
+                  decoration: const InputDecoration(
+                      hintText: 'broj komada', labelText: 'Rata'),
+                  validator: (value) {
+                    return validateNumber(value);
+                  },
+                  // onSaved: (String value) {
+                  //   _data.email = value;
+                  // }
                 ),
                 TextFormField(
-                    obscureText: true, // Use secure text for passwords.
-                    decoration: const InputDecoration(
-                        hintText: 'Password',
-                        labelText: 'Enter your password'
-                    ),
-                    // validator: this._validatePassword,
-                    // onSaved: (double.parse(value)) {
-                    //
-                    // }
+                  keyboardType: TextInputType.number,
+                  // Use email input type for emails.
+                  decoration: const InputDecoration(
+                      hintText: 'broj komada', labelText: 'Maksimalna nabava'),
+                  validator: (value) {
+                    return validateNumber(value);
+                  },
+                  // onSaved: (String value) {
+                  //   _data.email = value;
+                  // }
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  // Use email input type for emails.
+                  decoration: const InputDecoration(
+                      hintText: 'broj komada',
+                      labelText: 'Maksimalni kapacitet'),
+                  validator: (value) {
+                    return validateNumber(value);
+                  },
+                  // onSaved: (String value) {
+                  //   _data.email = value;
+                  // }
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  // Use email input type for emails.
+                  decoration: const InputDecoration(
+                      hintText: 'broj', labelText: 'Trošak nabave'),
+                  validator: (value) {
+                    return validateNumber(value);
+                  },
+                  // onSaved: (String value) {
+                  //   _data.email = value;
+                  // }
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  // Use email input type for emails.
+                  decoration: const InputDecoration(
+                      hintText: 'broj', labelText: 'Trošak skladištenja'),
+                  validator: (value) {
+                    return validateNumber(value);
+                  },
+                  // onSaved: (String value) {
+                  //   _data.email = value;
+                  // }
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                      hintText: 'primjer: 20 40 20',
+                      labelText: 'Broj razdoblja'),
+                  validator: (value) {
+                    return validateArray(value);
+                  },
+                  // onSaved: (double.parse(value)) {
+                  //
+                  // }
                 ),
                 Container(
                   width: displayWidth(context),
-                  child: RaisedButton(
+                  child: ElevatedButton(
                     child: const Text(
-                      'Login',
-                      style: TextStyle(
-                          color: Colors.white
-                      ),
+                      'Izračunaj',
+                      style: TextStyle(color: Colors.white),
                     ),
                     onPressed: submit,
-                    color: Colors.blue,
                   ),
-                  margin: const EdgeInsets.only(
-                      top: 20.0
-                  ),
+                  margin: const EdgeInsets.only(top: 20.0),
                 )
               ],
             ),
-          )
-      ),
+          )),
     );
+  }
+
+  String? validateArray(String? value) {
+    if (value == null || value.isEmpty) {
+      return "Unesite broj!";
+    } else {
+      List<String> lista = value.split(" ");
+      try {
+        for (String broj in lista) {
+          if (broj != "") {
+            int.parse(broj);
+          }
+        }
+        return null;
+      } catch (FormatException) {
+        return "Unesena stavka nije broj.";
+      }
+    }
+  }
+
+  String? validateNumber(String? value) {
+    if (value == null || value.isEmpty) {
+      return "Unesite broj!";
+    } else {
+      try {
+        int.parse(value);
+        return null;
+      } catch (FormatException) {
+        return "Unesena stavka nije broj.";
+      }
+    }
   }
 
   submit() {
